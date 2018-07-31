4d14b2e252fcd84565bf40f0db09024e0b17ed17
dembasy <dembasy1227@gmail.com>
2018-07-31 15:33:21 +0200

    done

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// tableau ou on stocke les mois de l'année
+$arrayMonths = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'décembre');
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title> exercice 1</title>
+  </head>
+  <body>
+  </body>
+</html>
